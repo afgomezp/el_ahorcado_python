6a05bcc7d26bbc3732237439537b97521379df4c
Felipe Gomez <afgomezp@gmail.com>
2021-12-23 14:53:42 -0500

    se hace un ensayo para verificar que la lista fue leida de manera correcta

--- ahoracado.py
+++ ahoracado.py
@@ -7,8 +7,22 @@ def archivo_read():
         for i in f.read().split('\n'):
             i.strip()
             lista_palabras.append(i)
+   
+
+    #llevar la lista de palabras a otro archivo
+    with open('./listaverificacion.txt', 'w', encoding ='utf-8') as f2:
+        for i in lista_palabras:
+            f2.write(i)
+            f2.write('$')
+            f2.write('\n')
+    
     f.close()
+    f2.close()
+
+
     return lista_palabras
+
+#llevar la lista de palabras a otro archivo
 
 
 def run():
