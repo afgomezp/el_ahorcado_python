09bc2947c2fe94082e3b1d8bb54859d12c049d83
Felipe Gomez <afgomezp@gmail.com>
2021-12-23 15:45:35 -0500

    a la funcion archivo read se le agrega que elija la palabra al azar y la duvuelva en return

--- ahoracado.py
+++ ahoracado.py
@@ -1,6 +1,8 @@
+#se importan los modulos que se van a usar
 import os
+import random
 
-#funcion para leer el archivo
+#funcion para leer el archivo y sacar palabra aleaotoria.
 def archivo_read():
     with open('./data.txt', 'r', encoding ='utf-8') as f:
         lista_palabras = []
@@ -8,12 +10,15 @@ def archivo_read():
             i.strip()
             lista_palabras.append(i)
     f.close()
-    return lista_palabras
+    random_word = random.choice(lista_palabras)
+    return random_word
 
 
 def run():
     #primero se limpa la consola.
     os.system('cls')
+
+    #se lee el archivo data.txt y se arroja la palabra aleatorioa
     print(archivo_read())
 
 
