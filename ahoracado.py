3ef2e9cf8d06fc57c5dc2b9430d8e889a3ec5bfa
Felipe Gomez <afgomezp@gmail.com>
2021-12-23 14:23:59 -0500

    se crea l

---
+++ ahoracado.py
@@ -0,0 +1,23 @@
+import os
+
+
+def archivo_read():
+    with open('./data.txt', 'r', encoding ='utf-8') as f:
+        lista_palabras = []
+        for i in f.read().split('\n'):
+            i.strip()
+            lista_palabras.append(i)
+    f.close()
+    return lista_palabras
+
+
+def run():
+    #primero se limpa la consola.
+    os.system('cls')
+    print(archivo_read())
+
+
+
+
+if __name__ =='__main__':
+    run()
