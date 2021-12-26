e329b3a3096d4d14fac3bd40f80ce226c1665e77
Felipe Gomez <afgomezp@gmail.com>
2021-12-26 17:08:36 -0500

    se ordena mejor el codigo y se crea la funcion interfaz

--- ahoracado.py
+++ ahoracado.py
@@ -13,27 +13,39 @@ def archivo_read():
     random_word = random.choice(lista_palabras)
     return random_word
 
-#se crea la funcion para mostrar el espacio de palabras.
-def espacios(my_dict,palabra):
-    if len(my_dict)==0:
-        print('_ '*len(palabra))
+#se crea la funcion para mostrar la interfaz del juego, se muestras espacios sin adivinar y letras adivinadas.
+def interfaz(my_dict,palabra):
+    os.system('cls')
+    print('Adivina la palabra oculta que se muestra a continuación:')
+    for i in range(len(palabra)):
+        if bool(my_dict.get(i))==True:
+            print(my_dict.get(i), end= " ")
+        else:
+            print ('_', end=" ")
+        
+    print('\n')
 
 
 #-------- aqui se se define la función principal del juego-------------------------------
 def run():
 
- #primero se limpa la consola.
-    os.system('cls')
-
  #se lee el archivo data.txt, se arroja la palabra aleatorioa y se asigna a una variable
     palabra =archivo_read()
-    print (palabra)
 
-#vamos a colocar la palabra dentro de una lista
+#vamos a colocar la palabra dentro de una lista y a enumerarla en otra lista
     my_list = [i for i in palabra]
    
-#vamos a crear una nueva lista que enumere los caracterteres de la palabra
-    lista_final = list(enumerate(my_list))
+    lista_final = list(enumerate(my_list))  
+    
+#se va a hacer un diccionario, donde en caso de que se acierte la letra, se guarde en la llave la posicion y en el valor la letra.
+    my_dict= {}
+
+#se corre la funcion de la interfaz.
+
+    interfaz(my_dict,palabra)
+
+
+
 
 #vamos a entrar en un ciclo para que el usuario verifique la palabra.
     my_dict={}
@@ -46,7 +58,7 @@ def run():
  # se va a colocar una afirmacion tipo assert preguntando si es string(true), o numero(false)
         assert not letra.isnumeric(),'solo se pueden ingresar letras, no números'
 
- #se va a hacer un diccionario, donde en caso de que se acierte la letra, se guarde en la llave la posicion y en el valor la letra.  
+ #se va llenar el diccionario previamente creado y llamado my_dict 
         for i, j in lista_final:
             if letra==j:
                 my_dict[i]=j
@@ -55,14 +67,7 @@ def run():
         print (my_dict)
         print (len(my_dict))
 
- # se crea una interfaz donde se muestra la posicion de las palabras acertadas
-        for i in range(len(palabra)):
-            if bool(my_dict.get(i))==True:
-                print(my_dict.get(i), end= " ")
-            else:
-                print ('_', end=" ")
-        
-        print('\n')
+
     
     
         
